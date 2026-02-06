48a9bb18e413b1679824bde16eef2bd929cc4fe2
azevedodaniel <daniel.ellerazevedo@gmail.com>
2026-02-06 12:44:40 -0300

    formatting the currency

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ amount.oninput = () => {
   // Obtém o valor atual do input e remove os caracteres não numéricos
   let value = amount.value.replace(/\D/g, "")
 
+  // Transformar o valor em centavos
+  value = Number(value)/100
+
   // Atualiza o valor do input
-  amount.value = value
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+  
+  // Retorna o valor formatado
+  return value
 }
