5f4678003db0bbec2679881333391bbebcd77fb9
azevedodaniel <daniel.ellerazevedo@gmail.com>
2026-04-29 14:49:25 -0300

    form usability

--- script.js
+++ script.js
@@ -95,6 +95,9 @@ function expenseAdd(newExpense){
     // Adiciona o item na lista
     expenseList.append(expenseItem)
 
+    // Limpa o formulário para adicionar um novo item
+    formClear()
+
     // Atualiza os totais
     updateTotals()
 
@@ -170,3 +173,13 @@ expenseList.addEventListener("click", function(event) {
   // Atualiza os totais
   updateTotals()
 })
+
+function formClear(){
+  // Limpa os inputs
+  expense.value = ""
+  category.value = ""
+  amount.value = ""
+
+  // Coloca o foco no input de amount
+  expense.focus()
+}
